615d4db193e4ed93e199fb65f9e05c2c89ee3b6f
Benjamin Monchatre <benjamin.monchatre@gmail.com>
2019-08-01 11:46:39 +0200

    final pour éval

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React,  { Component } from 'react';
 import shuffle from 'lodash.shuffle';
 import './App.css';
 import KeyboardEventHandler from 'react-keyboard-event-handler';
-import KeyBoard from './KeyBoard';
 
 const listemots = ['CHAT','LION','ZEBRE','ELEPHANT', 'VACHE','LAPIN','CHEVAL','CHIEN','RENARD','CROCODILE','SINGE','PERROQUET']
-
+const keyboard = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 
 class App extends Component {
@@ -18,8 +17,8 @@ class App extends Component {
             essai : 0,
             word : this.generateWord()
         }
-
     }
+
     reset= ()=> {
         this.setState(
                     {keyPressed : new Set([]),
@@ -30,77 +29,76 @@ class App extends Component {
         );
     }
 
-generateWord() {
-    return shuffle(listemots)[0]
-}
+    generateWord() {
+        return shuffle(listemots)[0]
+    }
 
-imageDisplay(numberImg) {
-    return (
-      <img src={require('./images/img'+ numberImg +'.png')} alt={numberImg} />
-    );
-}
+    imageDisplay(numberImg) {
+        return (
+          <img src={require('./images/img'+ numberImg +'.png')} alt={numberImg} />
+        );
+    }
 
-computeDisplay(phrase, usedLetters) {
-  return phrase.replace(/\w/g,
-    (letter) => (usedLetters.has(letter) ? letter : '_')
-  )
-}
+    computeDisplay(phrase, usedLetters) {
+      return phrase.replace(/\w/g,
+        (letter) => (usedLetters.has(letter) ? letter : '_')
+      )
+    }
 
-displayConsole(key) {
-   this.setState({keyPressed : this.state.keyPressed.add(key.toUpperCase())})
-
-   if (!this.state.word.includes(key.toUpperCase())&& this.state.essai!==this.state.maxEssai )
-   this.setState({essai : this.state.essai+1})
-}
-
-
+    handleKeyOrClick = key => {
+       this.setState({keyPressed : this.state.keyPressed.add(key.toUpperCase())})
+       if (!this.state.word.includes(key.toUpperCase())
+       && this.state.essai!==this.state.maxEssai
+       && !this.state.keyPressed.has(key.toUpperCase())
+       )
+       this.setState({essai : this.state.essai+1})
+    }
 
 render() {
 
-console.log('mot' +this.state.word)
 const phrase =this.computeDisplay(this.state.word,this.state.keyPressed)
 const lose= this.state.essai===this.state.maxEssai
 const win= !phrase.includes('_')
+
   return (
 
-
-
-         <div >
-
-         {!lose &&  <div className="keyboard">
-        <KeyboardEventHandler
-            handleKeys={['alphabetic']}
-            onKeyEvent={(key, e) => this.displayConsole(key)}
-            handleFocusableElements={true}
-             />
-          <KeyBoard />
-        </div>}
-
+ <div >
         <div>
-             {this.imageDisplay(this.state.essai+1)}
+             {this.imageDisplay(this.state.essai)}
+           <span> Mot à deviner :</span>
+           <span className="masque"> {phrase} </span>
+           <div> Essai restant : {this.state.maxEssai-this.state.essai}</div>
+           <br/>
         </div>
 
-
-        <div>
-            Try : {this.state.essai}
-        </div>
-
-         <div  className="phrase" >
-           <h1> {phrase} </h1>
-           {lose &&
-           <div>
-            <button onClick={this.reset}> recommencer </button>
-            <h1> vous avez perdu !!</h1>
-           </div>
-            }
-
-           {win && <h1> vous avez gagné !!</h1> }
-        </div>
+        <div> { (!win && !lose)? (
+                    <div className="keyboard">
+                          {
+                               keyboard.map((letter, index) => (
+                                 <button disabled={this.state.keyPressed.has(letter)}
+                                 className={this.state.keyPressed.has(letter)?"touch disabled":"touch"}
+                                   key={index}
+                                   index={index}
+                                   onClick={()=>this.handleKeyOrClick(letter)}
+                                 >{letter}</button>
+                               ))
+                          }
+                        <KeyboardEventHandler
+                            handleKeys={['alphabetic']}
+                            onKeyEvent={(key, e) => this.handleKeyOrClick(key)}
+                            handleFocusableElements={true}
+                        />
+                    </div>
+              ):(
+                    <div>
+                    <h1> vous avez {win?"gagné":"perdu"}!!</h1>
+                    <button onClick={this.reset}> recommencer </button>
+                    </div>
+                )
+        } </div>
  </div>
   );
-
 }
-
 }
 
 export default App;
